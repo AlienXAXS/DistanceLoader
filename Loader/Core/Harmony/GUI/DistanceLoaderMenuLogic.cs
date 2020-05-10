01d10d01d78762cfe10bece20d55c29e1ee16585
AlienXAXS <mark.phillips@axsemail.co.uk>
2020-05-10 22:05:20 +0100

    Added the About Screen

--- Loader/Core/Harmony/GUI/DistanceLoaderMenuLogic.cs
+++ Loader/Core/Harmony/GUI/DistanceLoaderMenuLogic.cs
@@ -32,11 +32,16 @@ namespace DistanceLoader.Core.Harmony.GUI
             this.menuPanelManager_ = G.Sys.MenuPanelManager_;
         }
 
-        public void OnClick()
+        public void DistanceLoaderSettings_OnClick()
         {
-            Util.Logger.Instance.Log("You clicked me!");
             menuPanelManager_.ShowTimedOk(0, "I'm sorry, this button does not do anything just yet!",
                 "Distance Mod Loader Settings", (MessagePanelLogic.OnButtonClicked) null, UIWidget.Pivot.Center);
+        }
+
+        public void DistanceLoaderAbout_OnClick()
+        {
+            menuPanelManager_.ShowTimedOk(0, "Distance Mod Loader\r\n-------------------------------\r\n\r\nCreated by: AlienX\r\nHelp from: Neon\r\n\r\nThis tool allows the game to be heavily modified, as if the game itself was running the modifications.",
+                "Distance Mod Loader About", (MessagePanelLogic.OnButtonClicked)null, UIWidget.Pivot.Center);
         }
     }
 }
